55107323f36de1bc4b93a7c0f84a7cc4aa9ded99
Laxman <laxmanketheth76@gmail.com>
2024-03-30 19:57:17 +0800

    random images are dropping and score is updating

--- frontend/src/components/GameScreen/GameScreen.jsx
+++ frontend/src/components/GameScreen/GameScreen.jsx
@@ -6,28 +6,87 @@ import { useRef } from 'react';
 
 const GameScreen = () => {
 
-    // const [score, setScore] = useState(0);
-
-    const [randomImgStyle, setRandomImgStyle] = useState({ bottom: '520px' });
-    // const [boatImgHeight, setBoatImgHeight] = useState({height: '120px'})
-    // console.log(randomImgStyle);
-    // console.log(boatImgHeight);
-
+    const [randomImgStyle, setRandomImgStyle] = useState({ bottom: '550px', left: '500px', display: 'block' });
+    const [boatStyles, setBoatStyles] = useState({ left: '500px', height: '120px' });
     const [randomImage, setRandomImage] = useState('');
     const [topScore, setTopScore] = useState(0);
-    const [leftStyles, setLeftStyles] = useState({ left: '500px' });
-    let parsedVal = parseInt(leftStyles.left);
-    // console.log(parsedVal);
+    const [score, setScore] = useState(0);
+  
+    // === using useRef hook === //
     let boatImgRef = useRef(null);
-    // let randomImgRef = useRef(null);
-    // console.log(randomImgRef.current);
+    let randomImgRef = useRef(null);
 
     //================= ImageArray to store image paths ==============//
     const ImgArray = ['/p1.png', '/p2.png', '/p3.png', '/p4.png', '/e1.png', '/e2.png']
-    const randomIndex = Math.floor(Math.random() * 6)
+    const randomIndex = Math.floor(Math.random() * 6);
 
 
-    //========== this useEffect is for fetching data ==========//
+    // ===making use of useState for scoreIncreased so that score increases only once===//
+    const [scoreIncreased, setScoreIncreased] = useState(false);
+
+
+    //=== below logic is to add 50 points or minus 100 points according to the image caught by boat ====//
+    const checkImgUrl = () => {
+        let randomImg = randomImgRef.current // .current is object property available on useRef hook//
+        let imgUrl = randomImg.src
+        let image1 = 'http://localhost:5173/e1.png'
+        let image2 = 'http://localhost:5173/e2.png'
+        if (imgUrl == image1 || imgUrl == image2) {
+            setScore(score -100);
+            setScoreIncreased(true);
+        } else {
+            setScore(score + 50);
+            setScoreIncreased(true);
+        }
+
+        // if(scoreIncreased){
+        //     setRandomImage(ImgArray[randomIndex]); 
+        // }
+        
+    };
+
+    //==========checkIntersection function is to check if the boat touched the random image==========//
+
+    //=== checkIntersection method checks the bottomValue of falling randomImg and checks it against the
+    //===  height of boatImg inside the if statement so that score can be updated ====//
+    const checkIntersection = () => {
+
+        //======== accessing random image properties ==========//
+        let randomImg = randomImgRef.current
+        let parsedBottomValue = parseInt(randomImg.style.bottom);
+        // let newDisplayProperty = 'none'
+        let ImgLeftValue = parseInt(randomImg.style.left)
+        // console.log('image left Value',ImgLeftValue);
+
+        //======== accessing boat image properties ==========//
+        let boatImg = boatImgRef.current
+        let parsedHeightValue = parseInt(boatImg.style.height);
+        let boatLeftValue = parseInt(boatImg.style.left)
+
+        //after the falling image touches the boat image height, the score is updated and the display of the falling image is set to none//
+        if (parsedHeightValue >= parsedBottomValue && ImgLeftValue === boatLeftValue && !scoreIncreased) {
+
+            //****** the function checkImgUrl is defined above ******//
+            checkImgUrl();
+
+            //******** updating display property of randomImgStyle using spread opeartor *******//
+            setRandomImgStyle(prevStyle => ({
+                ...prevStyle,
+                // display: newDisplayProperty
+                display : 'none'
+            }));
+
+            //******setting display back to 'block so that image dropping again is visible*****//
+            setRandomImgStyle((prevStyle) => ({
+                ...prevStyle,
+                display:'block'
+            }));
+            // console.log(randomImgStyle.bottom);
+        }
+    };
+
+
+    //========== this useEffect is for fetching data and setting highest score on page ==========//
     useEffect(() => {
         const fetchData = async () => {
             const response = await fetch('http://localhost:8080/')
@@ -45,45 +104,32 @@ const GameScreen = () => {
         //=== had to update randomImage state inside the useEffect because calling the setRandomImage function inside the component body triggers a re-render.
         // and it, in turn, causes the component to re-render infinitely, resulting in the error message.  
 
+         // =====this is for changing random image, it is kept here inside this useState because this useState has an
+         //=== empty dependency array which means it will render when the browser refreshes and hence it will give us random images when browser refresher === //
         setRandomImage(ImgArray[randomIndex]);
-
-        // =====this is for changing random image === //
-
-        //     const interval =  setInterval(() => {
-        //     // console.log('random index',randomIndex);
-        //     let image = ImgArray[randomIndex]
-        //     // console.log(randomIndex);
-        //     setRandomImage(image)
-        //     console.log(randomImage);
-        //    }, 1000);
-        //=========================================================================================//
-        //    return ()=> {
-        //     clearInterval(interval)
-        //    };
-        //==code above is for changing random image==//      
+        //=========================================================================================//   
 
     }, []);
 
 
-    //============== this useEffect is for arrow movement ================//
-    //=== By including leftStyles in the dependency array, the effect will be re-run whenever leftStyles changes===//
+    //============== this useEffect is for the movement of boat on arrow left and right ================//
+    //=== By including boatLeftStyle in the dependency array, the effect will be re-run whenever leftStyles changes===//
     useEffect(() => {
         const handleKeyPress = (e) => {
 
+            let parsedVal = parseInt(boatStyles.left);
+
             if (parsedVal > 0 && e.key === 'ArrowLeft') {
-                let boatImg = boatImgRef.current
-                // console.log('helo', boatImg.style.left);
+                let boatImg = boatImgRef.current // .current is object property available on useRef hook//
                 let parsedValue = parseInt(boatImg.style.left)
                 let newValue = parsedValue - 50 + 'px'
-                setLeftStyles({ left: newValue })
+                setBoatStyles({ left: newValue, height: boatStyles.height })
             }
             if (parsedVal < 1050 && e.key === 'ArrowRight') {
                 let boatImg = boatImgRef.current
                 let parsedValue = parseInt(boatImg.style.left)
-                // console.log(parsedValue);
                 let newValue = parsedValue + 50 + 'px'
-                setLeftStyles({ left: newValue })
-                // console.log(leftStyles);
+                setBoatStyles({ left: newValue, height: boatStyles.height })
             }
         };
 
@@ -92,31 +138,42 @@ const GameScreen = () => {
         return () => {
             window.removeEventListener('keydown', handleKeyPress);
         };
-    }, [leftStyles]);
+    }, [boatStyles]);
 
 
     //======= When we access the style property of an element immediately after rendering without using useEffect, it's possible that the element hasn't been fully rendered and
     //==  thats why we pass it inside useEffect to make sure the element has rendered at least once so that its value can be accessed.thus, we can access randomImgRef.current.style property==//
     //==otherwise  the initial value is null and if we access it immediately then it will be null or give errors//
 
-    let randomImgRef = useRef(null);
 
+
+    // =============== this useEffect is for dropping down the random image from top =============//
     useEffect(() => {
         let randomImgElement = randomImgRef.current
         // console.log(randomImgElement);
-        let parsedValue = parseInt(randomImgElement.style.bottom)
-        // console.log('top val is', parsedValue);
+        // need to parse the bottom value bcoz it is coming along with 'px' and we only need numeric value//
+        let imgBottomValue = parseInt(randomImgElement.style.bottom) 
         const interval = setInterval(() => {
-            let newBottomValue = parsedValue - 10 + 'px'
-            // console.log(randomImgStyle);
-            let updateBottomStyle = {bottom: newBottomValue}
-            console.log(updateBottomStyle);
-            setRandomImgStyle(updateBottomStyle)
-            // console.log('this is latest bottom style',randomImgStyle);
-            // let boatImg = boatImgRef.current 
-            // console.log('boatimg element',boatImg);
-            // let parsedValue = parseInt(boatImg.style.height)
-            // console.log('height of boat img',parsedValue);
+        let newBottomValue = imgBottomValue - 20 + 'px'
+
+            setRandomImgStyle((prevStyle) => ({
+                ...prevStyle,
+                bottom: newBottomValue
+            }));
+
+            //== the function checkIntersection is defined above ===//
+            checkIntersection();
+          
+            // below if statement to check so that random image starts dropping again from top //
+            if(imgBottomValue === -10){
+                setRandomImgStyle((prevStyle) => ({
+                    ...prevStyle,
+                    bottom: 550 + 'px'
+                }));
+                //when image bottom value is again made 550px as stated in above line then setRandomImage method will give another random image from top//
+                setRandomImage(ImgArray[randomIndex]); 
+            };
+           
 
         }, 100);
         return () => {
@@ -124,9 +181,6 @@ const GameScreen = () => {
         };
 
     }, [randomImgStyle])
-
-// console.log(randomImgStyle);
-
 
 
 
@@ -149,7 +203,7 @@ const GameScreen = () => {
                         />}
 
                         <img
-                            style={leftStyles}
+                            style={boatStyles}
                             ref={boatImgRef}
                             className='boatImg'
                             src="/boat.png"
@@ -161,7 +215,7 @@ const GameScreen = () => {
                 <div className="statsContainer">
                     <Link to={'/'}><button className='homebtn'>go home</button></Link>
                     <div className='yourScore'>Your Score
-                        <h3>123</h3>
+                        <h3>{score}</h3>
                         <Link to={'/saveScore'}> <button className='homebtn'>Save Score</button></Link>
                     </div>
                     <div className='highestScore'>Highest Score
