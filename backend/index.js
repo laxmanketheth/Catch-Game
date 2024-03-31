d72e7ef16c38de8666c47cd428766f5972c99364
Laxman <laxmanketheth76@gmail.com>
2024-03-31 10:59:50 +0800

    updated base url

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors(
     {
-        origin: '',
+        origin: 'http://localhost:5173',
         methods: ['POST', 'GET', 'DELETE'],
         credentials: true
     }
