62654824921f564b31041598c3cd96169feb7472
laxmanketheth <laxmanketheth76@gmail.com>
2025-01-01 23:24:57 +0800

    added log

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,9 @@ const cors = require('cors')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+console.log('hello');
+
 app.use(cors(
     {
         // origin: 'https://catch-game-frontend.vercel.app',
